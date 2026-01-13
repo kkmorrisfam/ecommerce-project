e0e072807dcf2494e3b0f51f8e8f27d36288453b
Kerri Morris <kkmorrisfam@hotmail.com>
2026-01-12 20:06:40 -0800

    Use axios to fetch the data

--- ecommerce/src/pages/HomePage.jsx
+++ ecommerce/src/pages/HomePage.jsx
@@ -5,12 +5,10 @@ import checkmark from "../assets/images/icons/checkmark.png";
 import "./HomePage.css";
 
 export function HomePage() {
-  fetch('http://localhost:3000/api/products')
+  axios.get('http://localhost:3000/api/products')
     .then((response)=>{
-      return response.json();
-      }).then((data)=>{
-        console.log(data);
-    });   
+      console.log(response.data);
+    });
   
   return (
     <>
