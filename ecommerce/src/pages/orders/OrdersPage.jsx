80ac34e85289d365a8e2277c18789fb0368ccbd8
Kerri Morris <kkmorrisfam@hotmail.com>
2026-01-14 16:58:54 -0800

    refactor into components from home page and checkout page

--- ecommerce/src/pages/orders/OrdersPage.jsx
+++ ecommerce/src/pages/orders/OrdersPage.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 import dayjs from "dayjs";
 import { useState, useEffect, Fragment } from "react";
-import { Header } from "../components/Header";
+import { Header } from "../../components/Header";
 import "./OrdersPage.css";
 import buy_again from "../assets/images/icons/buy-again.png";
-import { formatMoney } from "../utils/money";
+import { formatMoney } from "../../utils/money";
 
 export function OrdersPage({ cart }) {
   const [orders, setOrders] = useState([]);
