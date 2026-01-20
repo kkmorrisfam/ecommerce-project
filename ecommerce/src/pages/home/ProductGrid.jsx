a75dc4fe4a54471769437b0afe3e534b917675ab
Kerri Morris <kkmorrisfam@hotmail.com>
2026-01-19 20:05:55 -0800

    Separate product to update quantity

--- ecommerce/src/pages/home/ProductGrid.jsx
+++ ecommerce/src/pages/home/ProductGrid.jsx
@@ -1,8 +1,6 @@
 import { Product } from './Product';
 
 export function ProductsGrid({products, loadCart}) {
-  
-
   return (
     <div className="products-grid">
       {products.map((product) => {
