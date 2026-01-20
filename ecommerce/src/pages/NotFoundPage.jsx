07649558fb47990f7ee2a559254ef358f265db46
Kerri Morris <kkmorrisfam@hotmail.com>
2026-01-19 17:28:26 -0800

    add cart to 404 page

--- ecommerce/src/pages/NotFoundPage.jsx
+++ ecommerce/src/pages/NotFoundPage.jsx
@@ -1,13 +1,13 @@
 import { Header } from "../components/Header"
 import './NotFoundPage.css';
 
-export function NotFoundPage() {
+export function NotFoundPage({cart}) {
     return (
         <div className="my-page" >
             <link rel="icon" type="image/svg+xml" href="../assets/images/icons/checkmark.png" />
             <title>Oops!</title>
             <div>
-                <Header />
+                <Header cart={cart}/>
             </div>
             <div className="my-container">
                 <h2 className="h2" >What?</h2>            
