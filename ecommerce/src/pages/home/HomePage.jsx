21eadbaf280da767121880c6232e0bccfd139c25
Kerri Morris <kkmorrisfam@hotmail.com>
2026-01-19 19:48:14 -0800

    Create add to cart feature, update backend

--- ecommerce/src/pages/home/HomePage.jsx
+++ ecommerce/src/pages/home/HomePage.jsx
@@ -5,7 +5,7 @@ import { ProductsGrid } from './ProductGrid';
 import "./HomePage.css";
 
 
-export function HomePage({cart}) {
+export function HomePage({cart, loadCart}) {
   const [products, setProducts] = useState([]);
 
 
@@ -27,7 +27,7 @@ export function HomePage({cart}) {
       <Header cart={cart}/>
 
       <div className="home-page">
-        <ProductsGrid products={products}/>
+        <ProductsGrid products={products} loadCart={loadCart}/>
         
       </div>
     </>
