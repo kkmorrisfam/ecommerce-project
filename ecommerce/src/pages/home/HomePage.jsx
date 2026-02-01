f734888a3659f8cb86345024f8bca06b13978599
Kerri Morris <kkmorrisfam@hotmail.com>
2026-01-31 20:04:58 -0800

    add condition for no results from search

--- ecommerce/src/pages/home/HomePage.jsx
+++ ecommerce/src/pages/home/HomePage.jsx
@@ -1,46 +1,57 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
-import { ProductsGrid } from './ProductGrid';
-import { useSearchParams } from 'react-router';
+import { ProductsGrid } from "./ProductGrid";
+import { useSearchParams } from "react-router";
 import "./HomePage.css";
 
-
-
-export function HomePage({cart, loadCart}) {
+export function HomePage({ cart, loadCart }) {
   const [products, setProducts] = useState([]);
+  const [anyProducts, setAnyProducts] = useState(false);
 
   // get the search text from url
-  const [ searchParams ] = useSearchParams();
-  const search = searchParams.get('search');
+  const [searchParams] = useSearchParams();
+  const search = searchParams.get("search");
 
   useEffect(() => {
     const getHomeData = async () => {
-      let response; 
+      let response;
       if (search) {
-        response = await axios.get(`/api/products?search=${search}`)
-        
+        response = await axios.get(`/api/products?search=${search}`);
       } else {
-        response = await axios.get('/api/products');
-        
-      } 
-      setProducts(response.data);   
-    };    
-    
-    getHomeData();
-    
-  }, [search]);  //empty array means it will run once when page is created.
-  
+        response = await axios.get("/api/products");
+      }
 
+      //test if there are no products found
+      if (response.data && Object.keys(response.data).length === 0) {
+        console.log("there are no products found.");
+        setAnyProducts(false);
+      } else {
+         setAnyProducts(true);
+      }
+     
+      setProducts(response.data);
+    };
+
+    getHomeData();
+  }, [search]); //empty array means it will run once when page is created.
+
+  console.log(anyProducts);
+ 
   return (
     <>
+      
       <link rel="icon" type="image/svg+xml" href="/home-favicon.png" />
       <title>My Store</title>
-      <Header cart={cart}/>
+      <Header cart={cart} />
 
+      
       <div className="home-page">
-        <ProductsGrid products={products} loadCart={loadCart}/>
-        
+        {anyProducts ?
+        <ProductsGrid products={products} loadCart={loadCart} />
+          :
+        <h2>There are no products found. Try again.</h2>
+        }
       </div>
     </>
   );
