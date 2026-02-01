be3f6ed294fb2496df2dd1629f90e71940efd0c9
Kerri Morris <kkmorrisfam@hotmail.com>
2026-01-31 19:41:33 -0800

    add search functionality through api, redirect to homepage for search

--- ecommerce/src/components/Header.jsx
+++ ecommerce/src/components/Header.jsx
@@ -1,17 +1,50 @@
-import { NavLink } from 'react-router';
+import { NavLink, useNavigate } from 'react-router';
 import logo_white from '../assets/images/logo-white.png';
 import mobile_logo_white from '../assets/images/mobile-logo-white.png';
 import search_icon from '../assets/images/icons/search-icon.png';
 import cart_icon from '../assets/images/icons/cart-icon.png';
 
 import './Header.css';
+import { useState } from 'react';
+
 
 export function Header({cart}) {
+  const [searchString, setSearchString] = useState("");
+
+  const navigate = useNavigate();
+
   let totalQuantity = 0;
 
   cart.forEach((cartItem)=>{
     totalQuantity += cartItem.quantity;
   });
+
+  const searchThis = (event) => {
+    setSearchString(event.target.value);
+    // console.log("search", event.target.value);
+    // setTimeout(setSearchString(event.target.value), 6000)  
+    // console.log(searchString);
+
+  }
+
+  const handleSearchClick = () => {
+    console.log("Search string", searchString);
+    //navigate to the home page with search in url, 
+    // so homepage can use it
+    navigate(`/?search=${searchString}`)
+
+  }
+
+  const handleKeySearch = (event) => {
+    if (event.key === 'Enter') {
+      handleSearchClick();
+
+    } else if (event.key === 'Escape') {
+      setSearchString("");
+    }
+    
+    
+  }
 
   return (
     <div className="header">
@@ -23,10 +56,19 @@ export function Header({cart}) {
       </div>
 
       <div className="middle-section">
-        <input className="search-bar" type="text" placeholder="Search" />
+        <input 
+          className="search-bar" 
+          type="text" 
+          placeholder="Search" 
+          onClick={handleSearchClick}
+          onChange={searchThis}
+          onKeyDown={handleKeySearch}
+          />
 
         <button className="search-button">
-          <img className="search-icon" src={search_icon} />
+          <img className="search-icon" src={search_icon} 
+            onClick={handleSearchClick}
+            />
         </button>
       </div>
 
--- ecommerce/src/pages/home/HomePage.jsx
+++ ecommerce/src/pages/home/HomePage.jsx
@@ -2,22 +2,34 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Header } from "../../components/Header";
 import { ProductsGrid } from './ProductGrid';
+import { useSearchParams } from 'react-router';
 import "./HomePage.css";
+
 
 
 export function HomePage({cart, loadCart}) {
   const [products, setProducts] = useState([]);
 
+  // get the search text from url
+  const [ searchParams ] = useSearchParams();
+  const search = searchParams.get('search');
 
   useEffect(() => {
     const getHomeData = async () => {
-      const response = await axios.get('/api/products');
-      setProducts(response.data);    
+      let response; 
+      if (search) {
+        response = await axios.get(`/api/products?search=${search}`)
+        
+      } else {
+        response = await axios.get('/api/products');
+        
+      } 
+      setProducts(response.data);   
     };    
     
     getHomeData();
     
-  }, []);  //empty array means it will run once when page is created.
+  }, [search]);  //empty array means it will run once when page is created.
   
 
   return (
